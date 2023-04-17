509413cb69fdc01a5add4620cece2abe314b30c0
Andre-Vinicius22 <vinicius.bastos.monteiro@gmail.com>
2023-04-17 10:39:49 -0300

    show path on console

--- src/service/fileService.js
+++ src/service/fileService.js
@@ -3,13 +3,14 @@ import getFilePath from "../resource/getFilePath.js";
 const fileService = {
 	filePath: async () => {
 		const path = await getFilePath();
+		console.log(path);
 		const result = path.filter((element) => {
-			if(element.includes("nota_2023-03.pdf")) {
-				return element
+			if (element.includes("nota_2023-03.pdf")) {
+				return element;
 			}
-		})
-			if(result.length === 0){
-				throw new Error ("note not found!")
+		});
+		if (result.length === 0) {
+			throw new Error("note not found!");
 		}
 		return result;
 	},
