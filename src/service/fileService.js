8ce39b05fe9738895d2a4d0fd2ea02874410f843
GiovanaRamalho <giovanasr854@gmail.com>
2023-04-17 10:54:51 -0300

    refactor: fileService

--- src/service/fileService.js
+++ src/service/fileService.js
@@ -1,19 +1,22 @@
 import getFilePath from "../resource/getFilePath.js";
 
 const fileService = {
-	filePath: async () => {
-		const path = await getFilePath();
-		console.log(path);
-		const result = path.filter((element) => {
-			if (element.includes("nota_2023-03.pdf")) {
-				return element;
-			}
-		});
-		if (result.length === 0) {
-			throw new Error("note not found!");
-		}
-		return result;
-	},
+  filePath: async () => {
+    const path = await getFilePath();
+    console.log(path);
+    const folder = path.filter((element) => element.includes("2023"));
+
+    if (folder.length === 0) {
+      throw new Error("folder not found!");
+    }
+
+    const result = folder.filter((element) => element.includes("2023-04"));
+
+    if (result.length === 0) {
+      throw new Error("note not found!");
+    }
+    return result;
+  },
 };
 
 export default fileService;
