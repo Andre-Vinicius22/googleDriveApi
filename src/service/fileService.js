965dce8faf0f86819b26cf28218e9eb03e12d52b
Andre-Vinicius22 <vinicius.bastos.monteiro@gmail.com>
2023-04-17 11:11:23 -0300

    refactor: fileService

--- src/service/fileService.js
+++ src/service/fileService.js
@@ -1,22 +1,32 @@
 import getFilePath from "../resource/getFilePath.js";
 
 const fileService = {
-  filePath: async () => {
-    const path = await getFilePath();
-    console.log(path);
-    const folder = path.filter((element) => element.includes("2023"));
+	filePath: async () => {
+		const path = await getFilePath();
+		console.log(path);
 
-    if (folder.length === 0) {
-      throw new Error("folder not found!");
-    }
+		let date = new Date();
+		let year = date.getFullYear();
+		let mounth = date.getMonth() + 1;
 
-    const result = folder.filter((element) => element.includes("2023-04"));
+		const folder = path.filter((element) => element.includes(`${year}`));
 
-    if (result.length === 0) {
-      throw new Error("note not found!");
-    }
-    return result;
-  },
+		if (folder.length === 0) {
+			throw new Error("folder not found!");
+		}
+
+		const result = folder.filter(
+			(element) =>
+				element.includes(`${year}-0${mounth}`) ||
+				element.includes(`${year}/0${mounth}`) ||
+				element.includes(`${year}0${mounth}`)
+		);
+
+		if (result.length === 0) {
+			throw new Error("note not found!");
+		}
+		return result;
+	},
 };
 
 export default fileService;
