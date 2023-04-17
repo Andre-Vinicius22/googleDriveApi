7a82d3f70499f1beacf5326db1e146b657f2d63c
Andre-Vinicius22 <vinicius.bastos.monteiro@gmail.com>
2023-04-17 11:32:38 -0300

    refactor: dates atualized

---
+++ src/resource/dates.js
@@ -0,0 +1,7 @@
+let date = new Date();
+const dates = {
+	year: date.getFullYear(),
+	mounth: date.getMonth() + 1,
+};
+
+export default dates;
--- src/service/fileService.js
+++ src/service/fileService.js
@@ -1,15 +1,14 @@
 import getFilePath from "../resource/getFilePath.js";
+import dates from "../resource/dates.js";
 
 const fileService = {
 	filePath: async () => {
 		const path = await getFilePath();
 		console.log(path);
 
-		let date = new Date();
-		let year = date.getFullYear();
-		let mounth = date.getMonth() + 1;
-
-		const folder = path.filter((element) => element.includes(`${year}`));
+		const folder = path.filter((element) =>
+			element.includes(`${dates.year}`)
+		);
 
 		if (folder.length === 0) {
 			throw new Error("folder not found!");
@@ -17,12 +16,14 @@ const fileService = {
 
 		const result = folder.filter(
 			(element) =>
-				element.includes(`${year}`) && element.includes(`${mounth}`)
+				element.includes(`${dates.year}`) &&
+				element.includes(`${dates.mounth}`)
 		);
 
 		if (result.length === 0) {
 			throw new Error("note not found!");
 		}
+
 		return result;
 	},
 };
