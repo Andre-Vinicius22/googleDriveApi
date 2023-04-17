c63f0e2fc6e13fcccc3d2f2f0a0f4cf865036c38
Andre-Vinicius22 <vinicius.bastos.monteiro@gmail.com>
2023-04-17 11:18:49 -0300

    refactor: files delete

--- src/auth/googleAuth.js
+++
@@ -1,9 +0,0 @@
-import { google } from "googleapis";
-import keys from "./keys.js"; // credenciais da API
-
-export const googleDriveAuth = google.drive({
-	version: "v3",
-	auth: new google.auth.JWT(keys.client_id, null, keys.private_key, [
-		"https://www.googleapis.com/auth/drive",
-	]),
-});
