a15cdcd636aa4492e26f718ba725f062d2a41776
suelenadles <suelenadles@hotmail.com>
2023-04-19 14:17:57 -0300

    verify extention removed

--- src/service/fileService.js
+++ src/service/fileService.js
@@ -4,24 +4,29 @@ import dateService from "./dateService.js";
 const fileService = {
 	getFilePath: async () => {
 		const filePath = await getAuthFilePath();
-		console.log(filePath);
+		//console.log(filePath);
 
 		const verifyExtention =  /\.[^./\s]+$/g;
 		
 		const folderPath = filePath.filter((element) => 
-			element.includes(`${dateService.getCurrentYear()}`) && !verifyExtention.exec(element)
+			element.includes(`${dateService.getCurrentYear()}`)
 		);
-
+		
 		if (folderPath.length === 0) {
 			throw new Error("folder not found!");
 		}
-		
-		const fiscalNotePath = folderPath.filter((element) =>
-			element.includes(
-				`${dateService.getCurrentYear()}-0${dateService.getCurrentMounth()}`
-			)
-		);
+		console.log(folderPath);
 
+		const fiscalNotePath = folderPath.filter((element) => {
+		element.includes(
+			`${dateService.getCurrentYear()}-0${dateService.getCurrentMounth()}`
+			)
+		}
+			//console.log("element:", element);
+			//console.log(dateService.getCurrentYear());
+			//console.log(dateService.getCurrentMounth());
+			);
+			console.log(fiscalNotePath);
 		
 		if (fiscalNotePath.length === 0) {
 			throw new Error("note not found!");
