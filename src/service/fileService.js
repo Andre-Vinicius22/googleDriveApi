bbd447999df22bc618abd18fcfa0bae64b625eff
Andre-Vinicius22 <vinicius.bastos.monteiro@gmail.com>
2023-04-19 14:27:11 -0300

    fiscalNotePath filter fixed

--- src/service/fileService.js
+++ src/service/fileService.js
@@ -4,30 +4,23 @@ import dateService from "./dateService.js";
 const fileService = {
 	getFilePath: async () => {
 		const filePath = await getAuthFilePath();
-		//console.log(filePath);
+		console.log(filePath);
 
-		const verifyExtention =  /\.[^./\s]+$/g;
-		
-		const folderPath = filePath.filter((element) => 
+		// const verifyExtention = /\.[^./\s]+$/g;
+		const folderPath = filePath.filter((element) =>
 			element.includes(`${dateService.getCurrentYear()}`)
 		);
-		
+
 		if (folderPath.length === 0) {
 			throw new Error("folder not found!");
 		}
-		console.log(folderPath);
 
-		const fiscalNotePath = folderPath.filter((element) => {
-		element.includes(
-			`${dateService.getCurrentYear()}-0${dateService.getCurrentMounth()}`
+		const fiscalNotePath = folderPath.filter((element) =>
+			element.includes(
+				`${dateService.getCurrentYear()}-0${dateService.getCurrentMounth()}`
 			)
-		}
-			//console.log("element:", element);
-			//console.log(dateService.getCurrentYear());
-			//console.log(dateService.getCurrentMounth());
-			);
-			console.log(fiscalNotePath);
-		
+		);
+
 		if (fiscalNotePath.length === 0) {
 			throw new Error("note not found!");
 		}
